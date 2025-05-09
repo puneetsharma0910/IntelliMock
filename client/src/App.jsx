cae23d9a964d7a780a82fecf90ee1ebdd09253be
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-05-10 00:08:15 +0530

    Landing page and features section completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import Dashboard from "./pages/Home/Dashboard";
+import InterviewPrep from "./pages/InterviewPrep/InterviewPrep";
 const App = () => {
   return (
-    <div className='text-amber-900 bg-amber-600 text-9xl'>
-      hi
-      
-    </div>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/interview-prep/:sessionId"
+            element={<InterviewPrep />}
+          />
+        </Routes>
+      </BrowserRouter>
 
-export default App
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          success: {
+            theme: {
+              primary: "green",
+            },
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/pages/utils/data.js
@@ -0,0 +1,28 @@
+export const APP_FEATURES = [
+    {
+        id: "01",
+        title: "AI-Powered Interview Preparation",
+        description: "Get personalized interview preparation with our AI-powered platform.",
+    },
+    {
+        id: "02",
+        title: "AI-Powered Interview Preparation",
+        description: "Get personalized interview preparation with our AI-powered platform.",
+    },
+    {
+        id: "03",
+        title: "AI-Powered Interview Preparation",
+        description: "Get personalized interview preparation with our AI-powered platform.",
+    },
+    {
+        id: "04", 
+        title: "AI-Powered Interview Preparation",
+        description: "Get personalized interview preparation with our AI-powered platform.",
+    },
+    {
+        id: "05", 
+        title: "AI-Powered Interview Preparation",
+        description: "Get personalized interview preparation with our AI-powered platform.",
+    }
+
+];
