fd44512f197a4770586be0a58b8e5784d3ea949c
puneetsharma0910 <puneetsharma0910@gmail.com>
2025-05-11 21:40:39 +0530

    Refactor server-side code and improve authentication flow
    - Updated database configuration in `server/config/db.js`.
    - Enhanced authentication logic in `server/controllers/authController.js` and `server/middlewares/authMiddleware.js`.
    - Refined models: `Question.js`, `Session.js`, and `User.js`.

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  try
+  {
+    const token = req.headers.authorization;
+    if(token && token.startsWith("Bearer")) {
+        token = token.split(" ")[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+        next();
+    }
+    else {
+        res.status(401).json({message: "Not authorized, please login"});
+        
+    }
+  } catch (error) {
+    res.status(401).json({message: "Not authorized", error:error.message});
+  
+  }
+};
+
+module.exports = { protect };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 const path = require("path");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -11,6 +13,19 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
+
+connectDB();
+//routes
+// app.use("/api/auth", authRoutes);
+// app.use("/api/sessions", sessionRoutes);
+// app.use("/api/questions", questionRoutes);
+
+// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+
+
+
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
  const PORT = process.env.PORT || 5000;
